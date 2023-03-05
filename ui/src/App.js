e876fe1e373367d315c984cd4cc7eec0a1a56670
HGDrag <hristiyan.dragushkov@gmail.com>
2023-03-05 02:46:56 +0200

    navbar first version

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,12 @@
 import react from 'react';
 import Main from './components/Main';
 import './App.css';
-
+import Navbar from './components/Navbar';
 const App = () => { 
   
   return (
     <>
-    
+      <Navbar/>
       <Main />  
 
     </>
---
+++ ui/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+        <h2>Hriteria</h2>
+        <div className='nav_hamburger'>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </nav>
+  );
+}
+
+export default Navbar
