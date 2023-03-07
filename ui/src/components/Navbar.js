60892a8788ca03fcdbd24486db9f23516940651c
HGDrag <hristiyan.dragushkov@gmail.com>
2023-03-07 11:09:09 +0200

    toggle side menu

--- ui/src/components/Navbar.js
+++ ui/src/components/Navbar.js
@@ -1,15 +1,28 @@
 import React from 'react';
-
+import {useState} from 'react';
 const Navbar = () => {
+
+  const [menu, setMenu] = useState('hidden');
   return (
+    <>
     <nav className='nav'>
         <h2>Hriteria</h2>
-        <div className='nav_hamburger'>
+        <div className='nav_hamburger' onClick={() => setMenu('active')}>
             <div></div>
             <div></div>
             <div></div>
         </div>
     </nav>
+    <aside className={`sideNav ${menu}`} >
+      <h3 className='hideNav' onClick={() => setMenu('hidden')}>Menu</h3>
+        <div className='typesList'>
+          <a href='/'>Hot Drinks</a>
+          <a href='/'>Non-alcohol drinks</a>
+          <a href='/'>Alcohol drinks</a>
+          <a href='/'>Deserts</a>
+        </div>
+      </aside>
+    </> 
   );
 }
 
--- ui/src/components/ProductCard.js
+++ ui/src/components/ProductCard.js
@@ -5,7 +5,7 @@ export const ProductCard = (props) => {
   return (
 
     <div className='productCard'>
-        <img src = {props.img}alt='image of the product'></img>
+        <img src = {props.img}alt=' the product'></img>
         <div className='product_info'>
           <p>{props.type}</p>
           <h3>{props.name}</h3>
